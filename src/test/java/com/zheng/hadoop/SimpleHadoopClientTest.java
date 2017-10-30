7f82b59d0a9679c0c72903c303be1e940bf9f59b
zl13637992082 <736732419@qq.com>
2017-10-30 20:30:36 +0800

    hadoop study

--- src/test/java/com/zheng/hadoop/SimpleHadoopClientTest.java
+++ src/test/java/com/zheng/hadoop/SimpleHadoopClientTest.java
@@ -1,12 +1,14 @@
 package com.zheng.hadoop;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.net.URI;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * 1. 通过编程调用hadoop客户端API跟环境有关，如果是windows版本，那么需要编译hadoop支持windows
@@ -22,11 +24,12 @@ import java.net.URI;
  */
 public class SimpleHadoopClientTest {
     
-    private FileSystem fs = null;
+    private FileSystem fs;
+    private Configuration conf;
 
     @Before
     public void init() throws Exception {
-        Configuration conf = new Configuration();
+        conf = new Configuration();
         fs = FileSystem.get(new URI("hdfs://192.168.1.200:9000"), conf, "hadoop");
     }
     
@@ -40,6 +43,52 @@ public class SimpleHadoopClientTest {
     public void testDownload() throws Exception {
         fs.copyToLocalFile(false, new Path("/license.txt.copy"), new Path("d:/"), false);
         fs.close();
+    }
+    
+    @Test
+    public void testConf() {
+        Iterator<Map.Entry<String, String>> it = conf.iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, String> entry = it.next();
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+    
+    @Test
+    public void mkdir() throws Exception {
+        fs.mkdirs(new Path("/wordcount/input"));
+        fs.close();
+    }
+    
+    @Test
+    public void delete() throws Exception {
+        fs.delete(new Path("/wordcount/input"), true);
+        fs.close();
+    }
+    
+    @Test
+    public void testLs() throws Exception {
+        RemoteIterator<LocatedFileStatus> files = fs.listFiles(new Path("/"), true);
+        while (files.hasNext()) {
+            System.out.println("================================");
+            LocatedFileStatus status = files.next();
+            System.out.println("blocksize: " + status.getBlockSize());
+            System.out.println("accesstime: " + status.getAccessTime());
+            System.out.println("owner: " + status.getOwner());
+            System.out.println("replication: " + status.getReplication());
+            System.out.println("name: " + status.getPath().getName());
+            
+        }
+    }
+    
+    @Test
+    public void testLs2() throws IOException {
+        FileStatus[] statuses = fs.listStatus(new Path("/"));
+        for (FileStatus status : statuses) {
+            System.out.println("============================");
+            System.out.println("name: " + status.getPath().getName());
+            System.out.println(status.isDirectory() ? "directory" : "file");
+        }
     }
     
     
