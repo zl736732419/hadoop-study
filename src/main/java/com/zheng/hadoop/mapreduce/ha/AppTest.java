46fa3eaaa37b0e112484524a1a9548d12154cbc3
zl13637992082 <736732419@qq.com>
2017-11-18 23:14:52 +0800

    添加ha测试

---
+++ src/main/java/com/zheng/hadoop/mapreduce/ha/AppTest.java
@@ -0,0 +1,23 @@
+package com.zheng.hadoop.mapreduce.ha;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.net.URI;
+
+/**
+ * Created by zhenglian on 2017/11/18.
+ */
+public class AppTest {
+    
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://bi"), conf, "hadoop");
+        
+        fs.copyFromLocalFile(new Path("C:\\Users\\Administrator\\Desktop\\yarn-site.xml"), new Path("/yarn-site.xml"));
+        fs.close();
+    }
+    
+    
+}
